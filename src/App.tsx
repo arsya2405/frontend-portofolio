07f31c129e8d0e396ef42449e60eb529e5cd583e
arsya2405 <arsyanurafi@gmail.com>
2026-01-13 17:06:25 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const FooterComponent = () => {
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="fixed w-full">
+      <header className="sticky">
         <HeaderComponent />
       </header>
       <main className="flex-1">
