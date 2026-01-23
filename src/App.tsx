0d2970824c6b35f2e8ad95231660195caf267ce2
arsya2405 <arsyanurafi@gmail.com>
2026-01-23 15:45:49 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,11 +28,13 @@ function App() {
       <header className="sticky">
         <HeaderComponent />
       </header>
+
       <main className="flex flex-1 flex-col">
         <div>
           <p>Main</p>
         </div>
       </main>
+      
       <footer>
         <FooterComponent />
       </footer>
