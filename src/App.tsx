9cfbfbc389129baf617337d42087d146ac613a35
arsya2405 <arsyanurafi@gmail.com>
2026-01-26 22:13:13 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const HeaderComponent = () => {
       <nav>
         <ul className="flex gap-5 justify-center">
           {navigationItems.map((item) => (
-            <li className="rounded-xl p-2 text-white 
+            <li className="rounded-xl p-2 text-white font-semibold 
             hover:bg-blue-950 hover:text-shadow-[0_0_5px] 
             hover:text-shadow-white">
               <NavLink to={item.to}>{item.label}</NavLink>
