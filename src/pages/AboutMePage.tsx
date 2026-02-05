13d7f52744354d7283293f403a2eb709e7040b09
arsya2405 <arsyanurafi@gmail.com>
2026-02-05 17:05:25 +0700

    add Roboto font family

--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -1,6 +1,6 @@
 function AboutMe() {
     return (
-        <div className="flex flex-1 justify-center items-center">
+        <div className="flex flex-1 justify-center items-center p-5 gap-5">
             <div>picture</div>
             <div>text</div>
         </div>
