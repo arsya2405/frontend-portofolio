f2d8b2ab39fc52b299b2c2a07665d6726ef0965e
arsya2405 <arsyanurafi@gmail.com>
2026-02-05 12:07:53 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,11 @@ const navigationItems: NavigationItem[] = [
 const HeaderComponent = () => {
   return (
     <div className="bg-blue-800 p-5">
-      <nav className="flex gap-5 justify-center">
+      <nav className="flex gap-5 justify-center rounded-xl p-2 text-white font-semibold">
         {navigationItems.map((item) => (
-          <NavLink to={item.to} className="rounded-xl p-2 text-white font-semibold 
-          hover:underline
-          focus:no-underline
-          focus:text-shadow-[0_0_5px]
-          focus:text-shadow-white">{item.label}</NavLink>
+          <NavLink to={item.to} className={({ isActive }) => (
+            isActive ? "focus:no-underline focus:text-shadow-[0_0_5px] focus:text-shadow-white" : "hover:underline"
+          )}>{item.label}</NavLink>
         ))}
       </nav>
     </div>
