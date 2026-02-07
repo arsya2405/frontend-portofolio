6b196b7e55af8195dc9c6e932cf74f8f294ec8ff
arsya2405 <arsyanurafi@gmail.com>
2026-02-07 22:08:17 +0700

    change "Hello There!" font size

--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -2,7 +2,7 @@ function AboutMe() {
     return (
         <div className="flex flex-col p-5">
             <div>
-                Hello There!
+                <h1 className="text-[2rem]">Hello There!</h1>
             </div>
             <div className="flex flex-1 justify-center items-center gap-5">
                 <div>picture</div>
