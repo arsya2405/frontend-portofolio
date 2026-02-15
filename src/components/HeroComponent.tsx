873384115fca1c19ec7ef50ea2313d1ed5c0e763
arsya2405 <arsyanurafi@gmail.com>
2026-02-15 17:57:41 +0700

    created a new component called "HeroComponent"

---
+++ src/components/HeroComponent.tsx
@@ -0,0 +1,10 @@
+function Hero() {
+    return (
+        <div className="flex flex-col items-center">
+            <h1 className="text-[2rem]">Hello There!</h1>
+            <p>I'm <span className="font-bold">Muhammad Arsya Nurafi</span>, a Computer Science Student & Front-End Developer.</p>
+        </div>
+    );
+}
+
+export default Hero
