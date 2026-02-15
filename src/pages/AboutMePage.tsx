3729b243a3d1533bbe763d5c58fc8cb2a05f4f03
arsya2405 <arsyanurafi@gmail.com>
2026-02-15 17:40:24 +0700

    add <div> parent

--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -1,14 +1,16 @@
 function AboutMe() {
     return (
-        <article className="flex flex-col p-5">
-            <div>
-                <h1 className="text-[2rem]">Hello There!</h1>
-            </div>
-            <div className="flex flex-1 justify-center items-center gap-5">
-                <div>picture</div>
-                <div>text</div>
-            </div>
-        </article>
+        <div className="flex flex-col">
+            <article className="flex flex-col p-5">
+                <div>
+                    <h1 className="text-[2rem]">Hello There!</h1>
+                </div>
+                <div className="flex flex-1 justify-center items-center gap-5">
+                    <div>picture</div>
+                    <div>text</div>
+                </div>
+            </article>
+        </div>
     );
 }
 
