681d511f28aada67b1c08d3d6a54f3e3a693c764
arsya2405 <arsyanurafi@gmail.com>
2026-02-16 16:34:07 +0700

    Update HeroComponent.tsx

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -3,7 +3,7 @@ function Hero() {
         <div className="flex flex-col items-center">
             <h1 className="text-[2rem]">Hello There!</h1>
             <p>I'm <span className="font-bold">Muhammad Arsya Nurafi</span>, a Computer Science Student & Front-End Developer.</p>
-            <div className="flex px-10 py-2.5">
+            <div className="flex justify-center items-center px-10 py-2.5"> {/* size increases 1/4 */}
                 <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer" className="primary-button px-8 py-2"> {/* originally 8 and 2*/}
                     <strong>Contact Me</strong>
                 </a>
