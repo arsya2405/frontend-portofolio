833f7fff19bcf277a9ec8e6b6994179927efceb7
arsya2405 <arsyanurafi@gmail.com>
2026-02-16 16:14:45 +0700

    add div parent

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -3,9 +3,11 @@ function Hero() {
         <div className="flex flex-col items-center">
             <h1 className="text-[2rem]">Hello There!</h1>
             <p>I'm <span className="font-bold">Muhammad Arsya Nurafi</span>, a Computer Science Student & Front-End Developer.</p>
-            <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer" className="primary-button px-10 py-2.5"> {/* originally 8 and 2*/}
-                <strong>Contact Me</strong>
-            </a>
+            <div className="flex px-10 py-2.5">
+                <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer" className="primary-button px-8 py-2"> {/* originally 8 and 2*/}
+                    <strong>Contact Me</strong>
+                </a>
+            </div>
         </div>
     );
 }
