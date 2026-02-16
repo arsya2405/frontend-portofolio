39f9863706dc38ecc04925aa23969ac42c768e4a
arsya2405 <arsyanurafi@gmail.com>
2026-02-16 19:47:09 +0700

    changed the way of giving height and weight
    changed from setting padding to setting height and weight

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -3,10 +3,12 @@ function Hero() {
         <div className="flex flex-col items-center">
             <h1 className="text-[2rem]">Hello There!</h1>
             <p>I'm <span className="font-bold">Muhammad Arsya Nurafi</span>, a Computer Science Student & Front-End Developer.</p>
-            <div className="flex justify-center items-center px-10 py-2.5"> {/* size increases 1/4 */}
-                <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer" className="primary-button px-8 py-2"> {/* originally 8 and 2*/}
-                    <strong>Contact Me</strong>
-                </a>
+            <div className=""> {/* size increases 1/4 */}
+                <div className="primary-button w-[10rem] h-[3rem] flex items-center justify-center">
+                    <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer"> {/* originally 8 and 2*/}
+                        <strong>Contact Me</strong>
+                    </a>
+                </div>
             </div>
         </div>
     );
