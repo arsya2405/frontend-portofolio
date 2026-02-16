dce11574dba636c54cbc21b13e1bbfc6218fb0ed
arsya2405 <arsyanurafi@gmail.com>
2026-02-16 14:39:48 +0700

    make button round and bigger

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -3,7 +3,7 @@ function Hero() {
         <div className="flex flex-col items-center">
             <h1 className="text-[2rem]">Hello There!</h1>
             <p>I'm <span className="font-bold">Muhammad Arsya Nurafi</span>, a Computer Science Student & Front-End Developer.</p>
-            <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer" className="bg-[#3b82f6] text-white"><strong>Contact Me</strong></a>
+            <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer" className="bg-[#3b82f6] text-white px-8 py-2 rounded-4xl"><strong>Contact Me</strong></a>
         </div>
     );
 }
