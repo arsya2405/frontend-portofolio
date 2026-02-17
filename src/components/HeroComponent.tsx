7b01b8995f91cc9f659ee0499977596d8a4e41bb
arsya2405 <arsyanurafi@gmail.com>
2026-02-17 17:28:08 +0700

    add "LinkedIn" secondary button

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,13 +1,18 @@
 function Hero() {
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center gap-[0.5rem]">
             <h1 className="text-[2rem]">Hello There!</h1>
             <p>I'm <span className="font-bold">Muhammad Arsya Nurafi</span>, a Computer Science Student & Front-End Developer.</p>
-            <div className="button-parent w-[10rem] h-[2rem]"> {/* size increases 1/4 */}
+            <div className="button-parent w-[10rem] h-[2rem]">
                 <div className="primary-button">
-                    <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer"> {/* originally 8 and 2*/}
+                    <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer">
                         <strong>Contact Me</strong>
                     </a>
+                </div>
+            </div>
+            <div className="button-parent w-[10rem] h-[2rem]">
+                <div className="secondary-button">
+                    <a href="https://www.linkedin.com/in/muhammad-arsya-nurafi" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                 </div>
             </div>
         </div>
