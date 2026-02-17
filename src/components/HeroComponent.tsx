849b43af5c764839a554555a874c445ca5b0adae
arsya2405 <arsyanurafi@gmail.com>
2026-02-17 16:54:05 +0700

    convert styling from tailwind to CSS

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -3,8 +3,8 @@ function Hero() {
         <div className="flex flex-col items-center">
             <h1 className="text-[2rem]">Hello There!</h1>
             <p>I'm <span className="font-bold">Muhammad Arsya Nurafi</span>, a Computer Science Student & Front-End Developer.</p>
-            <div className="grid place-items-center w-[10rem] h-[2rem]"> {/* size increases 1/4 */}
-                <div className="primary-button w-full h-full flex items-center justify-center">
+            <div className="button-parent w-[10rem] h-[2rem]"> {/* size increases 1/4 */}
+                <div className="primary-button">
                     <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer"> {/* originally 8 and 2*/}
                         <strong>Contact Me</strong>
                     </a>
