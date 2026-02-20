697cae2deb2cfa32e1d56989059da2e2e9413fee
arsya2405 <arsyanurafi@gmail.com>
2026-02-20 16:41:16 +0700

    Update HeroComponent.tsx

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -4,7 +4,7 @@ function Hero() {
     return (
         <div className="flex flex-col items-center gap-[0.5rem]">
             <h1 className="text-[2rem]">Hello There!</h1>
-            <p>I'm <strong>Muhammad Arsya Nurafi</strong>, a Computer Science Student & Front-End Developer.</p>
+            <p>I'm <strong>Muhammad Arsya Nurafi</strong>, a Computer Science Student with a passion for Front-End Web Development.</p>
             <div className="primary-button w-[10rem] h-[2rem]">
                 <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer"><strong>Contact Me</strong></a>
             </div>
