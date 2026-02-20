dc2428c17a9340ee28edc2fc87c6e50b6f129c1e
arsya2405 <arsyanurafi@gmail.com>
2026-02-20 16:37:01 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const navigationItems: NavigationItem[] = [
 
 const HeaderComponent = () => {
   return (
-    <header className="sticky top-[0] z-[1000]">
+    <header className="sticky top-[0]">
       <nav className="primary-color p-5 flex gap-5 justify-center p-2 text-white font-semibold">
         {navigationItems.map((item) => (
           <NavLink to={item.to} className="nav-link">{item.label}</NavLink>
