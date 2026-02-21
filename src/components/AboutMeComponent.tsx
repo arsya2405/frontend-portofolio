bf6b18384264188c8943bd23258b539551952a88
arsya2405 <arsyanurafi@gmail.com>
2026-02-21 12:41:06 +0700

    created two columns in AboutMe component

---
+++ src/components/AboutMeComponent.tsx
@@ -0,0 +1,14 @@
+function AboutMe () {
+    return (
+        <article className="flex">
+            <div className="flex-1">
+                
+            </div>
+            <div className="flex-1">
+
+            </div>
+        </article>
+    )
+}
+
+export default AboutMe
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,11 @@
 import Hero from "../components/HeroComponent";
+import AboutMe from "../components/AboutMeComponent";
 
 function Home() {
     return (
         <section className="flex flex-col gap-[20px]">
             <Hero />
-            
+            <AboutMe />
         </section>
     );
 }
