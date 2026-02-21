3c82dde04bd1db3242ba935a7554a1ec86bb3040
arsya2405 <arsyanurafi@gmail.com>
2026-02-21 13:45:18 +0700

    set font size for h2 elements

--- src/components/AboutMeComponent.tsx
+++ src/components/AboutMeComponent.tsx
@@ -2,7 +2,7 @@ function AboutMe () {
     return (
         <article className="flex">
             <div className="flex-1">
-                
+                <h2>About Me</h2>
             </div>
             <div className="flex-1">
 
