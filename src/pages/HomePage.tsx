18c00687825bb04644a44bbb66feda716b656773
arsya2405 <arsyanurafi@gmail.com>
2026-02-21 12:24:42 +0700

    changed from <div> to semantic <section> element

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,14 +2,14 @@ import Hero from "../components/HeroComponent";
 
 function Home() {
     return (
-        <div className="flex flex-col gap-[20px]">
+        <section className="flex flex-col gap-[20px]">
             <article>
                 <Hero />
             </article>
             <article>
                 <Hero />
             </article>
-        </div>
+        </section>
     );
 }
 
