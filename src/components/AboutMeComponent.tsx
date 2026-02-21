81f5af2721649b0e1b42b216d0692210b64ad7c7
arsya2405 <arsyanurafi@gmail.com>
2026-02-21 16:09:36 +0700

    Update AboutMeComponent.tsx

--- src/components/AboutMeComponent.tsx
+++ src/components/AboutMeComponent.tsx
@@ -2,7 +2,7 @@ function AboutMe () {
     return (
         <article className="flex">
             <div className="flex-1 flex flex-col gap-[0.5rem]">
-                <h2 className="underline">About Me</h2>
+                <h1 className="underline">About Me</h1>
                 <ul className="list-disc list-inside">
                     <li>Possess strong web and game development skills with fluency in English.</li>
                     <li>Keen to pursue a career in Information Technology (IT) with a focus on front-end web developer roles.</li>
