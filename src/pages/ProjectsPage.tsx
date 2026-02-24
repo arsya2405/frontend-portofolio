80db7955a21c3d26181209f999882c34afa98ff6
arsya2405 <arsyanurafi@gmail.com>
2026-02-24 14:31:35 +0700

    added ProjectItem interface

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,4 +1,13 @@
 function Projects() {
+    interface ProjectItem {
+        img: string;
+        title: string;
+        creator: string;
+        description: string;
+        techStack: string[];
+        score?: string;
+    }
+
     return (
         <section className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <h1>Projects</h1>
