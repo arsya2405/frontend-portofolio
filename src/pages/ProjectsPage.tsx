ef35c6b276d413c17fd0c2968ade8a9aa8b3e375
arsya2405 <arsyanurafi@gmail.com>
2026-02-24 15:38:13 +0700

    added the first "projects" object

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,5 +1,8 @@
+import project1 from "../assets/images/switch-project.avif"
+
 function Projects() {
     interface ProjectItem {
+        id: string;
         img: string;
         title: string;
         creator: string;
@@ -9,14 +12,44 @@ function Projects() {
         projectURL: string;
     }
 
+    const projects: ProjectItem[] = [
+        {
+            id: "switch2",
+            img: project1, 
+            title: "Nintendo Switch 2 Information Website", 
+            creator: "Myself",
+            description: [
+                "Designed and built a website containing information regarding the Nintendo Switch 2 console.",
+                "Utilized HTML to define the structure of the webpage and also CSS to apply visuals and responsiveness on it."
+            ], 
+            techStack: ["HTML", "CSS"], 
+            score: "4 / 5 Stars",
+            projectURL: "https://drive.google.com/file/d/1YJwzxsHgx22FeknLcAwehWYipO89EQXw/view?usp=drive_link"
+        },
+        /*
+        {
+            id: "",    
+            img: "", 
+            title: "", 
+            creator: "", 
+            description: ["",], 
+            techStack: ["",], 
+            score: "97/100", 
+            projectURL: ""
+        },
+        */
+    ]
+    
     return (
         <section className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="flex flex-col">
-                <img src="" alt="" id="project-image" className=""/>
-                <div id="project-details" className="flex flex-col">
+            {projects.map((project) => (
+                <div id={project.id} className="flex flex-col">
+                    <img src={project.img} alt={project.title} className=""/>
+                    <div id="project-details" className="flex flex-col">
 
+                    </div>
                 </div>
-            </div>
+            ))}
         </section>
     );
 }
