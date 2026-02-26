f60b05e11f0652ca2ea3d9d732a577b827a98bb1
arsya2405 <arsyanurafi@gmail.com>
2026-02-26 20:56:45 +0700

    delete dummy card

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -20,23 +20,6 @@ function Projects() {
             creator: "Myself",
             descriptions: [
                 "Designed and built a website containing information regarding the Nintendo Switch 2 console.",
-                "Utilized HTML to define the structure of the webpage and also CSS to apply visuals and responsiveness on it.",
-                "Designed and built a website containing information regarding the Nintendo Switch 2 console.",
-                "Utilized HTML to define the structure of the webpage and also CSS to apply visuals and responsiveness on it.",
-                "Designed and built a website containing information regarding the Nintendo Switch 2 console.",
-                "Utilized HTML to define the structure of the webpage and also CSS to apply visuals and responsiveness on it.",
-            ], 
-            techStack: ["HTML", "CSS"], 
-            score: "4 / 5 Stars",
-            projectURL: "https://drive.google.com/file/d/1YJwzxsHgx22FeknLcAwehWYipO89EQXw/view?usp=drive_link"
-        },
-        {
-            id: "switch2",
-            img: project1, 
-            title: "Nintendo Switch 2 Information Website", 
-            creator: "Myself",
-            descriptions: [
-                "Designed and built a website containing information regarding the Nintendo Switch 2 console.",
                 "Utilized HTML to define the structure of the webpage and also CSS to apply visuals and responsiveness on it."
             ], 
             techStack: ["HTML", "CSS"], 
@@ -56,19 +39,6 @@ function Projects() {
             score: "4 / 5 Stars",
             projectURL: "https://drive.google.com/file/d/1YJwzxsHgx22FeknLcAwehWYipO89EQXw/view?usp=drive_link"
         },
-        {
-            id: "switch2",
-            img: project1, 
-            title: "Nintendo Switch 2 Information Website", 
-            creator: "Myself",
-            descriptions: [
-                "Designed and built a website containing information regarding the Nintendo Switch 2 console.",
-                "Utilized HTML to define the structure of the webpage and also CSS to apply visuals and responsiveness on it."
-            ], 
-            techStack: ["HTML", "CSS"], 
-            score: "4 / 5 Stars",
-            projectURL: "https://drive.google.com/file/d/1YJwzxsHgx22FeknLcAwehWYipO89EQXw/view?usp=drive_link"
-        }
         /*
         {
             id: "",    
