4d18cca1e31c5d22e23fde1b3e2b1e9b3a2c9943
arsya2405 <arsyanurafi@gmail.com>
2026-02-28 23:02:58 +0700

    Update HeroComponent.tsx
    the removed flex stylings from CSS before became tailwind classes

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -2,14 +2,14 @@ import linkedinIcon from '../assets/images/linkedin-logo.avif'
 
 function Hero() {
     return (
-        <article id="hero" className="flex flex-col items-center gap-[0.5rem]">
+        <article id="hero" className="flex flex-col items-center gap-2">
             <p className='text-[3rem] text-center'>Hello There!</p>
             <p className="text-center">I'm <strong>Muhammad Arsya Nurafi</strong>, a Computer Science Student & Aspiring Front-End Developer based in South Jakarta, Indonesia.</p>
-            <div className="primary-button w-[10rem] h-[2rem]">
+            <div className="primary-button w-30 flex justify-center">
                 <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer"><strong>Contact Me</strong></a>
             </div>
-            <div className="secondary-button w-[10rem] h-[2rem]">
-                <img src={linkedinIcon} alt="LinkedIn Icon" className="button-icon"/>
+            <div className="secondary-button flex items-center gap-1 w-30 flex justify-center">
+                <img src={linkedinIcon} alt="LinkedIn Icon" className="h-4"/>
                 <a href="https://www.linkedin.com/in/muhammad-arsya-nurafi" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             </div>
         </article>
