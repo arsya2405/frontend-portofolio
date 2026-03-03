2b1be409aba5e9c827f30cc9b75c4e2d2a29a62f
arsya2405 <arsyanurafi@gmail.com>
2026-03-03 10:59:47 +0700

    Update HeroComponent.tsx
    made primary and secondary button have exact same height

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -5,10 +5,10 @@ function Hero() {
         <article id="hero" className="flex flex-col items-center gap-2">
             <p className='text-[3rem] text-center'>Hello There!</p>
             <p className="text-center">I'm <strong>Muhammad Arsya Nurafi</strong>, a Computer Science Student & Aspiring Front-End Developer based in South Jakarta, Indonesia.</p>
-            <div className="primary-button w-30 flex justify-center">
+            <div className="primary-button w-30 h-6 flex justify-center">
                 <a href="mailto:arsyanurafi2405@gmail.com" target="_blank" rel="noopener noreferrer"><strong>Contact Me</strong></a>
             </div>
-            <div className="secondary-button flex items-center gap-1 w-30 flex justify-center">
+            <div className="secondary-button flex items-center gap-1 w-30 h-6 justify-center">
                 <img src={linkedinIcon} alt="LinkedIn Icon" className="h-4"/>
                 <a href="https://www.linkedin.com/in/muhammad-arsya-nurafi" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             </div>
