f5a5cccb776ad761262b7c84541fe71b0eac0bc2
arsya2405 <arsyanurafi@gmail.com>
2026-03-11 15:21:06 +0700

    Update NotFoundPage.tsx
    added a button to return to homepage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,13 @@
+import { NavLink } from 'react-router-dom';
+
 function NotFound() {
     return (
-        <h1>404 - Page Not Found</h1>
+        <div className="h-full w-full flex flex-col items-center gap-2">
+            <h1 className="text-[#a0a0a0]">404 - Page Not Found :(</h1>
+            <div className="primary-button w-35 h-10 flex justify-center items-center">
+                <NavLink to="/"><strong>Return to Home</strong></NavLink>
+            </div>
+        </div>
     );
 }
 
