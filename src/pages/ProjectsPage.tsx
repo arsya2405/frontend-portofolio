ff6df7c821a6dca174df88d5ffc388d41b56dc05
arsya2405 <arsyanurafi@gmail.com>
2026-03-11 14:24:58 +0700

    Update ProjectsPage.tsx
    added functionality to the read more/less button

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,9 +1,10 @@
 import project1 from "../assets/images/switch-project.avif"
 import project2 from "../assets/images/blotting-blob-screenshot.avif"
+import scoreIcon from "../assets/images/Plain_Yellow_Star.avif";
 import { programmingLanguages } from "../App"
 import { frameworks } from "../App";
 import { tools } from "../App";
-import scoreIcon from "../assets/images/Plain_Yellow_Star.avif";
+import { useState } from "react";
 
 function Projects() {
     interface ProjectItem {
@@ -70,12 +71,19 @@ function Projects() {
         <section className="flex flex-col gap-4 w-full">
             <h1>My Projects</h1>
             <article className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start">
-                {projects.map((project) => (
-                    <div id={project.id} className="p-10 rounded-4xl border shadow-[0_5px_5px_#808080] flex flex-col gap-4">
+                {projects.map((project) => {
+                    const [isExpanded, setIsExpanded] = useState(false);
+
+                    const expandOrShorten = () => {
+                        setIsExpanded(!isExpanded);
+                    };
+
+                   return (
+                    <div id="short-card" className="p-5 rounded-4xl border shadow-[0_5px_5px_#808080] flex flex-col gap-4 relative">
                         <div className="aspect-video overflow-hidden">
                             <img src={project.img} alt={project.title} className="object-cover h-full w-full rounded-4xl border"/>
                         </div>
-                        <div id="project-details" className="flex flex-col gap-4 items-start">
+                        <div id="project-details" className="flex flex-col gap-2 items-start">
                             <div className="flex flex-col w-full">
                                 <div id="title-score" className="flex flex-wrap justify-between items-center">
                                     <h2>{project.title}</h2>
@@ -84,6 +92,9 @@ function Projects() {
                                         <p><strong>{project.score}</strong></p>
                                     </div>
                                 </div>
+                            </div> 
+                            {isExpanded ? 
+                            <div className="flex flex-col gap-4 items-start">
                                 <div id="creator-date-url" className="flex flex-wrap gap-2">
                                     <p>Created by <strong>{project.creator}</strong></p>
                                     <span>|</span>
@@ -91,30 +102,34 @@ function Projects() {
                                     <span>|</span>
                                     <a href={project.projectURL} target="_blank" rel="noopener noreferrer" className="link-text">{project.urlLabel}</a>
                                 </div>
-                            </div> 
-                            <div id="tech-stack" className="flex flex-wrap gap-2">
-                                {project.techStack.map((tech) => (
-                                    <div className=
-                                    {programmingLanguages.includes(tech) ? "programming-language-pill" : 
-                                    frameworks.includes(tech) ? "framework-pill" :
-                                    tools.includes(tech) ? "tool-pill" : "generic-pill bg-[#e0e0e0]"}>
-                                        {tech}
-                                    </div>
-                                ))}
-                            </div>
-                            <ul id="description" className="list-disc list-inside">
-                                {project.descriptions.map((desc) => (
-                                    <li>{desc}</li>
-                                ))}
-                            </ul>
-                            <div id="read-more-or-less" className="flex justify-center w-full">
-                                <div className="primary-button w-30 flex justify-center">
-                                    <strong>↑ Read Less</strong>
+                                <div id="tech-stack" className="flex flex-wrap gap-2">
+                                    {project.techStack.map((tech) => (
+                                        <div className=
+                                        {programmingLanguages.includes(tech) ? "programming-language-pill" : 
+                                        frameworks.includes(tech) ? "framework-pill" :
+                                        tools.includes(tech) ? "tool-pill" : "generic-pill bg-[#e0e0e0]"}>
+                                            {tech}
+                                        </div>
+                                    ))}
                                 </div>
+                                <ul id="description" className="list-disc list-inside">
+                                    {project.descriptions.map((desc) => (
+                                        <li>{desc}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            :
+                            <div className="absolute"></div>
+                            }
+                            <div id="read-more-or-less" className="flex justify-center w-full">
+                                <button onClick={expandOrShorten} className="primary-button w-30 flex justify-center">
+                                    <strong>{isExpanded ? "↑ Read Less" : "↓ Read More"}</strong>
+                                </button>
                             </div>
                         </div>
                     </div>
-                ))}
+                   );
+                })}
             </article>
         </section>
     );
