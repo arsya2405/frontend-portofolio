bec151b7d52c0ac7e2d1a71f497c5f2cbf096112
arsya2405 <arsyanurafi@gmail.com>
2026-03-12 16:32:57 +0700

    Update NotFoundPage.tsx

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 function NotFound() {
     return (
-        <div className="h-full w-full flex flex-col items-center gap-2">
+        <div className="h-full w-full flex flex-col items-center gap-4">
             <h1 className="text-[#a0a0a0]">404 - Page Not Found :(</h1>
             <div className="primary-button w-35 h-10 flex justify-center items-center">
                 <NavLink to="/"><strong>Return to Home</strong></NavLink>
