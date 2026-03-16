ab7c58bcb549c18eeb538fc9a2ddbd8476b2576f
arsya2405 <arsyanurafi@gmail.com>
2026-03-16 15:38:27 +0700

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import AboutMe from "../components/AboutMeComponent";
 
 function Home() {
     return (
-        <section className="flex flex-col flex-1 gap-20">
+        <section className="flex flex-col flex-1 gap-10">
             <Hero />
             <AboutMe />
         </section>
