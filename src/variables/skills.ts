314a9143bf0ee5c4770c845faa4e22c378e2595d
arsya2405 <arsyanurafi@gmail.com>
2026-03-25 12:40:23 +0700

    Update skills.ts
    added new tool

--- src/variables/skills.ts
+++ src/variables/skills.ts
@@ -1,4 +1,4 @@
 export const programmingLanguages: string[] = [ "TypeScript", "HTML/CSS", "SQL" ];
 export const frameworks: string[] = [ "React", "Tailwind CSS" ];
-export const tools: string[] = [ "GitHub Desktop" ];
+export const tools: string[] = [ "GitHub Desktop", "Chrome DevTools" ];
 export const softSkills: string[] = [ "Being Detail-Oriented", "Problem-Solving", "Collaboration", "Time Management" ];
